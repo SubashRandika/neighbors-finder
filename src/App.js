bb4e809e013bddb26043a9b73e5bcfa685d73591
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-08-07 11:06:29 +0530

    Implemented fetch ten random countries.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [randomCountries, setRandomCountries] = useState();
+
+	useEffect(() => {
+		const getRandom = async (array, number) => {
+			const result = new Array(number);
+			let len = array.length;
+			const taken = new Array(len);
+
+			if (number > len) {
+				throw new RangeError('More elements going to take than available');
+			}
+
+			while (number--) {
+				const random = Math.floor(Math.random() * len);
+				result[number] = array[random in taken ? taken[random] : random];
+				taken[random] = --len in taken ? taken[len] : len;
+			}
+
+			return result;
+		};
+
+		const fetchRandomCountries = async () => {
+			const response = await fetch('https://travelbriefing.org/countries.json');
+			const countries = await response.json();
+			const tenRandomCountries = await getRandom(countries, 10);
+			console.log(tenRandomCountries);
+			setRandomCountries(tenRandomCountries);
+		};
+
+		fetchRandomCountries();
+	}, []);
+
+	return (
+		<div>
+			<h1 className='app-title'>Neighbors Finder</h1>
+			<button className='btn-generate'>Generate Groupings</button>
+		</div>
+	);
 }
 
 export default App;
